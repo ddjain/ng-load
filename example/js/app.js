cb9d5d271c176c719e3715c48f782e681a387ac2
Darshan Jain <nnd.darshan@gmail.com>
2019-03-23 14:50:15 +0530

    Update app.js

--- example/js/app.js
+++ example/js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', ['ngLoad']);
-	app.controller('myCtrl', function($scope) {
-		$scope.iFrameCallbackFunction = function() {
-            alert("Iframe is Loaded successfully")
-		};
-	});  
+
+app.controller('myCtrl', function($scope) {
+	$scope.iFrameCallbackFunction = function() {
+           alert("Iframe is Loaded successfully")
+	};
+});  
